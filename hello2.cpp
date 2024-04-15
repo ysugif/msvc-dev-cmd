ee069751a55c95490a4e7ca43e77910efcb4aff1
yuki sugiura <y.sugiura@fairydevices.jp>
2024-04-15 14:47:17 +0900

    pipe example.

--- hello2.cpp
+++ hello2.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
   std::string str;
   
-  whiel( std::cin >> str ) {
+  while( std::cin >> str ) {
     std::cout << "Hello2[" << str << "]" << std::endl;
   }
   return 0;
