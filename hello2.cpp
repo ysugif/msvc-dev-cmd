2ed9b01f2d438249ad3ab9bb12ebe2b3701c9ab5
yuki sugiura <y.sugiura@fairydevices.jp>
2024-04-15 14:39:13 +0900

    pipe example.

---
+++ hello2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main(int argc, char **argv)
+{
+  std::string str;
+  
+  std::cin << str;
+  std::cout << "Hello2[" << str << "]" << std::endl;
+  return 0;
+}
+// updated
