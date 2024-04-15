c9da0adb76092bb5b0f1edf5b5cef7f2d0ce72ed
yuki sugiura <y.sugiura@fairydevices.jp>
2024-04-15 16:35:35 +0900

    use picojson.

--- hello2.cpp
+++ hello2.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
+#include "picojson.h"
 
 int main(int argc, char **argv)
 {
   std::string str;
-  
+
+  picojson::array a;
   while( std::cin >> str ) {
-    std::cout << "Hello2[" << str << "]" << std::endl;
+    a.push_back(picojson::value(str);
   }
+  std::cout << "Hello2[" << picojson::value(a) << "]" << std::endl;
   return 0;
 }
 // updated
